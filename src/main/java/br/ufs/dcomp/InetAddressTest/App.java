d48133bc5894bd83a24c293d13d8809508fbaaa1
Elton Santos <elton.santos@dcomp.ufs.br>
2017-11-28 19:28:17 +0000

    Inclusão do método getAllByName()

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -13,9 +13,20 @@ public class App
         try{
             
             String name = "www.ufs.br";
-            InetAddress address = InetAddress.getByName(name); 
+            //InetAddress address = InetAddress.getByName(name); 
+            //System.out.println( "Name:      "+ name);
+            //System.out.println( "Address:   "+ address.getHostAddress());
+            
+            InetAddress[] address = InetAddress.getAllByName(name); 
             System.out.println( "Name:      "+ name);
-            System.out.println( "Address:   "+ address);
+            
+            for (int i = 0; i < address.length; i++) {
+            //for (int i = 0; address[i] != null; i++) {
+                
+                System.out.println( "Address:   "+ address[i].getHostAddress());
+                
+            }
+            
 
         } catch (UnknownHostException e){
             System.out.println(e.getMessage());
